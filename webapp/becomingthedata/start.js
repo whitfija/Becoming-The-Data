49e8c6eac8bf2f521f17687a3af259bda5c6b322
James Whitfield <whitfija@gmail.com>
2024-03-07 05:52:41 -0500

    map page, font additions, logo fix
    added infrastructure for map page, added fonts according to figma, added logo from figma

--- webapp/becomingthedata/start.js
+++ webapp/becomingthedata/start.js
@@ -7,10 +7,18 @@ const app = express()
 app.engine('ejs', ejs.renderFile);
 app.set("view engine", "ejs")
 
+app.use('/public', express.static('public'))
+app.use(express.urlencoded({extended: true}))
+
+
 // index
 app.get('/', (req, res)=>{
-    const filePath = path.resolve(__dirname, 'views/pages/index.html');
-    res.sendFile(filePath);
+    res.render("pages/index");
+})
+
+// map
+app.get('/map', (req, res)=>{
+    res.render("pages/map");
 })
 
 app.listen(3000, ()=>{
